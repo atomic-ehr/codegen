699688c1f6db2b27a248c8f891ba78c0ccd294ea
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2025-09-26 14:07:32 +0200

    test: move (todo) tests from fhir-clj/type-schema

---
+++ test/unit/typeschema/transformer/constraint.test.ts
@@ -0,0 +1,119 @@
+import { describe, expect, it } from "bun:test";
+import type { PFS } from "../../../../test/unit/typeschema/utils";
+import { fs2ts, r4 } from "../../../../test/unit/typeschema/utils";
+
+describe("TypeSchema Transformer Core Logic", () => {
+  const A: PFS = {
+    url: "A",
+    derivation: "specialization",
+    name: "a",
+    elements: {
+      foo: {
+        type: "BackboneElement",
+        elements: {
+          bar: { type: "string" },
+        },
+      },
+    },
+  };
+
+  it.todo("Check optional choice fields", async () => {
+    expect(await fs2ts(r4, A)).toMatchObject([
+      {
+        identifier: { kind: "resource", name: "a", url: "A" },
+        fields: {
+          foo: { type: { kind: "nested", name: "foo", url: "A#foo" } },
+        },
+        nested: [
+          {
+            identifier: { kind: "nested", name: "foo", url: "A#foo" },
+            base: {
+              kind: "complex-type",
+              name: "BackboneElement",
+              url: "http://hl7.org/fhir/StructureDefinition/BackboneElement",
+            },
+            fields: {
+              bar: {
+                type: {
+                  kind: "primitive-type",
+                  name: "string",
+                  url: "http://hl7.org/fhir/StructureDefinition/string",
+                },
+              },
+            },
+          },
+        ],
+        dependencies: [
+          {
+            kind: "complex-type",
+            name: "BackboneElement",
+            url: "http://hl7.org/fhir/StructureDefinition/BackboneElement",
+          },
+          {
+            kind: "primitive-type",
+            name: "string",
+            url: "http://hl7.org/fhir/StructureDefinition/string",
+          },
+          null,
+        ],
+      },
+      null,
+    ]);
+  });
+
+  const B: PFS = {
+    base: "A",
+    url: "B",
+    name: "b",
+    derivation: "constraint",
+    elements: { foo: { min: 1 } },
+  };
+
+  it.todo("Check optional choice fields", async () => {
+    expect(await fs2ts(r4, A)).toMatchObject([
+      {
+        identifier: { kind: "constraint", name: "b", url: "B" },
+        base: { kind: "resource", name: "a", url: "A" },
+        fields: {
+          foo: { type: { kind: "nested", name: "foo", url: "A#foo" } },
+        },
+        nested: "nil?",
+        dependencies: [
+          { kind: "resource", name: "a", url: "A" },
+          { kind: "nested", name: "foo", url: "A#foo" },
+          null,
+        ],
+      },
+      null,
+    ]);
+  });
+
+  const C: PFS = {
+    base: "B",
+    url: "C",
+    name: "c",
+    derivation: "constraint",
+    elements: { foo: { max: 1 } },
+  };
+
+  describe("Choice type translation", () => {
+    it.todo("Check optional choice fields", async () => {
+      expect(await fs2ts(r4, A)).toMatchObject([
+        {
+          identifier: { kind: "constraint", name: "c", url: "C" },
+          base: { kind: "constraint", name: "b", url: "B" },
+          fields: {
+            foo: { type: { kind: "nested", name: "foo", url: "A#foo" } },
+          },
+          nested: "nil?",
+          dependencies: [
+            { kind: "constraint", name: "b", url: "B" },
+            { kind: "nested", name: "foo", url: "A#foo" },
+            null,
+          ],
+        },
+        null,
+      ]);
+    });
+  });
+});
---
+++ test/unit/typeschema/transformer/hierarchy.test.ts
@@ -0,0 +1,112 @@
+import { describe, expect, it } from "bun:test";
+import type { PFS } from "../../../../test/unit/typeschema/utils";
+import { fs2ts, r4 } from "../../../../test/unit/typeschema/utils";
+
+describe("TypeSchema Transformer Core Logic", () => {
+  const A: PFS = {
+    url: "A",
+    elements: {
+      foo: { type: "string", array: true },
+    },
+  };
+
+  const B: PFS = {
+    base: "A",
+    url: "B",
+    required: ["foo"],
+    elements: {
+      foo: { min: 1 },
+      bar: { type: "code" },
+    },
+  };
+
+  const C: PFS = {
+    base: "B",
+    url: "C",
+    required: ["bar", "baz"],
+    elements: {
+      foo: { max: 2 },
+      baz: { type: "string" },
+    },
+  };
+
+  describe("Choice type translation", () => {
+    it.todo("Check optional choice fields", async () => {
+      expect(await fs2ts(r4, A)).toMatchObject([
+        {
+          identifier: { url: "A" },
+          fields: {
+            foo: {
+              excluded: false,
+              type: { name: "string" },
+              array: true,
+              required: false,
+            },
+          },
+          dependencies: [{ name: "string" }],
+        },
+        null,
+      ]);
+    });
+    it.todo("Check optional choice fields", async () => {
+      expect(await fs2ts(r4, A)).toMatchObject([
+        {
+          identifier: { url: "B" },
+          base: { url: "A" },
+          fields: {
+            foo: {
+              type: { name: "string" },
+              required: true,
+              excluded: false,
+              array: true,
+              min: 1,
+            },
+            bar: {
+              excluded: false,
+              type: { name: "code" },
+              array: false,
+              required: false,
+            },
+          },
+          dependencies: [{ name: "code" }, { name: "string" }, null],
+        },
+        null,
+      ]);
+    });
+    it.todo("Check optional choice fields", async () => {
+      expect(await fs2ts(r4, A)).toMatchObject([
+        {
+          identifier: { url: "C" },
+          base: { url: "B" },
+          fields: {
+            foo: {
+              excluded: false,
+              type: { name: "string" },
+              array: true,
+              min: 1,
+              max: 2,
+              required: true,
+            },
+            baz: {
+              excluded: false,
+              type: { name: "string" },
+              array: false,
+              required: true,
+            },
+            bar: null,
+          },
+          dependencies: [
+            {
+              kind: "primitive-type",
+              package: "hl7.fhir.r4.core",
+              version: "4.0.1",
+              name: "string",
+              url: "http://hl7.org/fhir/StructureDefinition/string",
+            },
+          ],
+        },
+        null,
+      ]);
+    });
+  });
+});
