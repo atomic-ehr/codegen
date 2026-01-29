30456a6fee888127742b24d4a986629abd5c1037
MikhailArtemyev <grumblemisha@gmail.com>
2026-01-29 16:40:15 +0000

    test: fixed ccda.test.ts

--- src/api/writer-generator/python.ts
+++ src/api/writer-generator/python.ts
@@ -165,12 +165,8 @@ export class Python extends Writer<PythonGeneratorOptions> {
 
     private generateRootPackages(groups: TypeSchemaPackageGroups): void {
         this.generateRootInitFile(groups);
-        if (this.forFhirpyClient)
-            this.copyAssets(
-                resolvePyAssets("fhirpy_base_model.py"),
-                Path.resolve(this.opts.outputDir, "fhirpy_base_model.py"),
-            );
-        this.copyAssets(resolvePyAssets("requirements.txt"), Path.resolve(this.opts.outputDir, "requirements.txt"));
+        if (this.forFhirpyClient) this.copyAssets(resolvePyAssets("fhirpy_base_model.py"), "fhirpy_base_model.py");
+        this.copyAssets(resolvePyAssets("requirements.txt"), "requirements.txt");
     }
 
     private generateSDKPackages(groups: TypeSchemaPackageGroups): void {
