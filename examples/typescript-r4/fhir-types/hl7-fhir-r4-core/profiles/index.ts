7c7aeec20e40124771e27c8455099fc14d7f0200
Artyom Bologov <git@aartaka.me>
2026-02-09 19:14:56 +0400

    Move R4 observation vitalsigns to profile index

--- examples/typescript-r4/fhir-types/hl7-fhir-r4-core/profiles/index.ts
+++ examples/typescript-r4/fhir-types/hl7-fhir-r4-core/profiles/index.ts
@@ -1,2 +1,3 @@
+export type { observation_vitalsigns } from "./Observation_vitalsigns";
 export { Observation_bodyweightProfile } from "./Observation_bodyweight";
 export { Observation_vitalsignsProfile } from "./Observation_vitalsigns";
