8394731b28f62c5f6dfb8be1052689864ed31df2
Aleksandr Penskoi <alexandr.penskoi@health-samurai.io>
2026-03-05 10:32:43 +0100

    Remove treeshaken files from US Core example

--- examples/typescript-us-core/fhir-types/hl7-fhir-uv-sdc/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./profiles";
-export type { SDCExample } from "./SDCExample";
-export type { SDCQuestionLibrary, sdc_question_libraryAddress, sdc_question_libraryName } from "./SDCQuestionLibrary";
